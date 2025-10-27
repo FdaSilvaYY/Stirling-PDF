c1e1eef3404781e01c9ec7b8928f5daf74c5a76d
James Brunton <james@stirlingpdf.com>
2025-10-27 12:11:20 +0000

    Remove proprietary styles from core app

--- frontend/src/proprietary/App.tsx
+++ frontend/src/proprietary/App.tsx
@@ -12,6 +12,7 @@ import OnboardingTour from "@app/components/onboarding/OnboardingTour";
 import "@app/styles/tailwind.css";
 import "@app/styles/cookieconsent.css";
 import "@app/styles/index.css";
+import "@app/styles/auth-theme.css";
 
 // Import file ID debugging helpers (development only)
 import "@app/utils/fileIdSafety";
